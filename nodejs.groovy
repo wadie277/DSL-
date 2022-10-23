1f5028fb339ed92c315e64e929e9cc86f56bc1ff
wadie277 <116502681+wadie277@users.noreply.github.com>
2022-10-23 10:51:30 -0400

    Create nodejs.groovy

---
+++ nodejs.groovy
@@ -0,0 +1,20 @@
+job('NodeJS example') {
+    scm {
+        git('https://github.com/namanjainyr/ChatApp.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Naman Jain')
+            node / gitConfigEmail('namanjainyr@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
+
+   
